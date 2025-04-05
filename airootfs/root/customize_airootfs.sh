ae24ff9da870adf066fd1c47cbf7aba7b45b0d2e
SublimateTheBerry <nepan.8066@gmail.com>
2025-04-05 17:36:11 +0300

    bugfix.. again

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Auto-start X
 echo '[[ $(tty) == "/dev/tty1" ]] && startx' >> /etc/skel/.bash_profile
 
-# Calamares autostart
 cat > /etc/skel/.xinitrc << EOF
 #!/bin/sh
 exec calamares -d
 EOF
 
-# Enable services
-systemctl enable sddm NetworkManager
+ln -s /usr/lib/systemd/system/sddm.service /etc/systemd/system/display-manager.service
+ln -s /usr/lib/systemd/system/NetworkManager.service /etc/systemd/system/multi-user.target.wants/
